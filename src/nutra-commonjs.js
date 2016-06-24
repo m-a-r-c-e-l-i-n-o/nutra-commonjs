f5940175e59e94b0fe2df4643ab456d105269db4
Marceli.no <me@marceli.no>
2016-06-24 14:24:05 -0400

    fix(core): Make sure the module loader runs on Node 4+
    The module loader relies on a npm module called "really-need"
    to intercept and instrument files, however, it's not
    compatible with major versions of Node. This dependency was
    switched for another module called "require-hacker", which
    seems to work on all versions past 4.0.0. Versions below
    4.0.0 were not tested.

--- src/nutra-commonjs.js
+++ src/nutra-commonjs.js
@@ -1,14 +1,20 @@
-import ReallyNeed from 'really-need'
-require = ReallyNeed
+import Fs from 'fs'
+import requireHacker from 'require-hacker'
 
 const moduleloader = (events, system, opts) => {
+    const cache = {}
     events.onLoad = () => {
-        system.files.forEach(function (file) {
-            require(file, {
-                bust: true,
-                pre: system.callbacks.onFileSourceLoaded
-            })
+        const hook = requireHacker.hook('js', (filename) => {
+            if (cache[filename]) {
+                return cache[filename]
+            }
+            return cache[filename] = system.callbacks.onFileSourceLoaded(
+                Fs.readFileSync(filename, 'utf8'),
+                filename
+            )
         })
+        system.files.forEach((file) => require(file))
+        hook.unmount()
     }
     events.onExit = () => {}
 }
