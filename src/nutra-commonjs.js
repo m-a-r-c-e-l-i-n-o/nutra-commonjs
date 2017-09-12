b1f1864033b4e66c2377efacbb8f47dc88bf8a17
Marceli.no <me@marceli.no>
2017-09-12 01:51:36 -0400

    feat: Adding first class support for Chrome dev tools

--- src/nutra-commonjs.js
+++ src/nutra-commonjs.js
@@ -1,16 +1,32 @@
-import Fs from 'fs'
+import { join } from 'path'
+import { readFileSync } from 'fs'
 import requireHacker from 'require-hacker'
 
 const moduleloader = (events, system, opts) => {
     const cache = {}
     events.onLoad = () => {
-        const hook = requireHacker.hook('js', (filename) => {
-            if (cache[filename]) {
-                return cache[filename]
+        const hook = requireHacker.global_hook('*', (filename, module) => {
+            if (!filename.endsWith('.js')) return
+
+            const { tmpDirectory: tmp } = system
+            const { filename: name } = module
+            if (name.indexOf(tmp) === 0) {
+                const pluginName = name.slice(tmp.length + 1)
+                const baseName = pluginName
+                    .slice(pluginName.indexOf('/'))
+                    .replace(/\|/g, '/')
+                    .slice(0, -2)
+
+                module.filename = join(system.basePath, baseName)
             }
-            return cache[filename] = system.callbacks.onFileSourceLoaded(
-                Fs.readFileSync(filename, 'utf8'),
-                filename
+
+            const resolvedName = requireHacker.resolve(filename, module)
+            if (cache[resolvedName]) {
+                return cache[resolvedName]
+            }
+            return cache[resolvedName] = system.callbacks.onFileSourceLoaded(
+                readFileSync(resolvedName, 'utf8'),
+                resolvedName
             )
         })
         system.files.forEach((file) => require(file))
